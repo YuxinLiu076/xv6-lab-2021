a875b4a68467eb1215ca468328973a9028d26acd
yuxinliu <lyx1141491623@gmail.com>
2023-06-27 21:24:18 -0700

    [Yuxin]: add solution for sleep.c

--- user/sleep.c
+++ user/sleep.c
@@ -1,13 +1,20 @@
 #include "kernel/types.h"
 #include "user/user.h"
 
-int main(int argc, char *argv[]){
-	if(argc < 1){
-		fprintf(2, "usage: sleep pattern[times]\n");
-		exit(1);
-	}
-	sleep(atoi(argv[1]));
-	exit(0);
+int main(int argc, char *argv[]) {
+    if (argc < 1) {
+        fprintf(2, "usage: sleep pattern[times]\n");
+        exit(1);
+    }
+//    printf("[0] %s, [1] %s, argc: %d", argv[0], argv[1], argc);
+    // argv[0]: sleep, argv[1] is the sleep ticks
+    int time = atoi(argv[1]);
+    sleep(time);
+//    int ret = sleep(time);
+//    if (ret != 0) {
+//        fprintf(2, "Error in sleep sys_call!\n");
+//    }
+    exit(0);
 }
 
 
